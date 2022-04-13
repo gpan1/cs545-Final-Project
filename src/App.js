5535b754d780b94c7a0f1fe8fe9046f6107e8fd9
Nikky5236 <57335776+Nikky5236@users.noreply.github.com>
2022-04-13 17:17:27 -0400

    trimmed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, Component } from "react";
+import Title from "./components/Title.js";
+import SweetAlert from "sweetalert-react";
+import Button from "./components/Button.js";
+import Answer from "./components/Answer.js";
+import Hiragana from "./syllabary/Hiragana.js";
+import Katakana from "./syllabary/Katakana.js";
+import Character from "./components/Character.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	state = {
+		alertText: "",
+		alertTitle: "",
+		alertActive: false,
+		alertType: "success",
+		characters: Object.assign(Hiragana, Katakana),
+		currentCharacter: "ア",
+	};
+
+	randomCharacter(characters) {
+		let result;
+		let count = 0;
+		Object.keys(characters).map(character => {
+			if (Math.random() < 1 / ++count) result = character;
+		});
+		return result;
+	}
+
+	checkAnswer = answer => {
+		if (answer == this.state.characters[this.state.currentCharacter]) {
+			this.setState({
+				currentCharacter: this.randomCharacter(this.state.characters),
+			});
+		} else {
+			this.setState({
+				alertType: "error",
+				alertTitle: "Woops",
+				alertText: `${this.state.currentCharacter} is "${this.state.characters[this.state.currentCharacter]}"`,
+				alertActive: true,
+				currentCharacter: this.randomCharacter(this.state.characters),
+			});
+		}
+	};
+
+	render() {
+		return (
+			<div>
+				<Fragment>
+					<SweetAlert
+						type={this.state.alertType}
+						text={this.state.alertText}
+						title={this.state.alertTitle}
+						show={this.state.alertActive}
+						onConfirm={() => this.setState({ alertActive: false })}
+					/>
+					<Title>Guess The Character</Title>
+					<Character>{this.state.currentCharacter}</Character>
+					<Answer handler={this.checkAnswer} />
+				</Fragment>
+			</div>
+		);
+	}
 }
-
-export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default styled.button`
+	color: #fff;
+	cursor: pointer;
+	font-size: 0.7em;
+	padding: 1em 3em;
+	margin-bottom: 1em;
+	border-radius: 3px;
+	transition: 0.3s ease;
+	border: 3px solid #fff;
+	background: transparent;
+	&:hover {
+		color: #3023ae;
+		background: #fff;
+	}
+`;
