d4fadb4682a20ceb1eaa75df8809e9f20ce82b41
Nikky5236 <57335776+Nikky5236@users.noreply.github.com>
2022-04-14 20:20:41 -0400

    animated health bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Fragment, Component } from "react";
 import Title from "./components/Title.js";
 import SweetAlert from "sweetalert-react";
-import Button from "./components/Button.js";
 import Answer from "./components/Answer.js";
 import Hiragana from "./syllabary/Hiragana.js";
 import Katakana from "./syllabary/Katakana.js";
 import Character from "./components/Character.js";
+import { Line, Circle } from "rc-progress";
 
 export default class App extends Component {
 	state = {
@@ -15,6 +15,8 @@ export default class App extends Component {
 		alertType: "success",
 		characters: Object.assign(Hiragana, Katakana),
 		currentCharacter: "ア",
+		health: 100, // base health will decrement eac time and double with each correct answer set
+		healthDecrement: 50,
 	};
 
 	randomCharacter(characters) {
@@ -26,10 +28,23 @@ export default class App extends Component {
 		return result;
 	}
 
+	updateHealth() {
+		let newHealth = this.state.health - this.state.healthDecrement;
+		if (newHealth <= 0) {
+			this.setState({
+				healthDecrement: Math.floor(this.state.healthDecrement * 0.8),
+			});
+			return 100;
+		} else {
+			return this.state.health - this.state.healthDecrement;
+		}
+	}
+
 	checkAnswer = answer => {
 		if (answer == this.state.characters[this.state.currentCharacter]) {
 			this.setState({
 				currentCharacter: this.randomCharacter(this.state.characters),
+				health: this.state.health - 25, // don't know if this will work
 			});
 		} else {
 			this.setState({
@@ -40,6 +55,10 @@ export default class App extends Component {
 				currentCharacter: this.randomCharacter(this.state.characters),
 			});
 		}
+		// this.setState({
+		// 	currentCharacter: this.randomCharacter(this.state.characters),
+		// 	health: this.updateHealth(), // don't know if this will work
+		// });
 	};
 
 	render() {
@@ -56,6 +75,7 @@ export default class App extends Component {
 					<Title>Guess The Character</Title>
 					<Character>{this.state.currentCharacter}</Character>
 					<Answer handler={this.checkAnswer} />
+					<Line percent={this.state.health} strokeWidth="4" strokeColor="#D3D3D3" />
 				</Fragment>
 			</div>
 		);
