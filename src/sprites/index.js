38c90b5eb40489016578fde56e50ac3443e7beb4
Gavin Pan <gpan1@stevens.edu>
2022-04-26 22:38:40 -0400

    Doinb Ryze Hack? 英雄联盟
    added more sprites

--- src/sprites/index.js
+++ src/sprites/index.js
@@ -2,7 +2,9 @@ import albanana from "./albanana.png";
 import albanana2 from "./albanana2.png";
 import albanana3 from "./albanana3.png";
 import sanbean from "./sanbean.png";
+import lyndonbee from "./lyndonbee"
+import bellLad from "./bellLad"
 
-let sprites = { albanana, albanana2, albanana3, sanbean };
+let sprites = { albanana, albanana2, albanana3, sanbean, lyndonbee, bellLad};
 
 module.exports = sprites;
