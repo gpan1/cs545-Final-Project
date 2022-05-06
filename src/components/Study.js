3fe85c0fcba2e9f50a57726034f54078cb0823fa
Adam Szyluk <aszyluk@stevens.edu>
2022-05-05 21:43:03 -0400

    Added routing for future study mode
    Used react-router-dom and refactored code to accommodate the existence of another mode, which we will call study mode.

---
+++ src/components/Study.js
@@ -0,0 +1,7 @@
+import React, { Fragment, Component } from "react";
+import Hiragana from "../syllabary/Hiragana.js";
+import Katakana from "../syllabary/Katakana.js";
+
+function Study() {}
+
+export default Study;
