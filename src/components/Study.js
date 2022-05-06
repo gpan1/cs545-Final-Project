6847f1f9740c38a54f039d62fac88b1fb45c7bff
Adam Szyluk <aszyluk@stevens.edu>
2022-05-05 22:03:34 -0400

    Study mode first implementation
    Study mode technically functions properly, but it needs a serious facelift.

--- src/components/Study.js
+++ src/components/Study.js
@@ -1,7 +1,51 @@
-import React, { Fragment, Component } from "react";
+import React, { useState } from "react";
 import Hiragana from "../syllabary/Hiragana.js";
 import Katakana from "../syllabary/Katakana.js";
 
-function Study() {}
+function Study() {
+  const [isShown, setIsShown] = useState(false);
+  let characters = Object.assign(Hiragana, Katakana);
+
+  return (
+    <div>
+      {Object.keys(characters).map((char) => {
+        return (
+          <div>
+            <button
+              onMouseEnter={() => setIsShown(true)}
+              onMouseLeave={() => setIsShown(false)}
+            >
+              {char}
+            </button>
+            {isShown && <div>{characters[char]}</div>}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 
 export default Study;
+
+/*
+function App() {
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <div className="App">
+      {charList.map((char) => {
+        <button
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}>
+          char.key
+        </button>
+        {isShown && (
+          <div>
+            char.value
+          </div>
+        )}
+      }
+    </div>
+  );
+}
+*/
